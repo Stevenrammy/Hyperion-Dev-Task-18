b1d5f65462a02b96a08dd364b41d68a89e74c6db
Steven Ramsbottom <stevenrammy@hotmail.co.uk>
2024-03-16 00:58:20 +0000

    Changing file to one with errors

--- Example_File.py
+++ Example_File.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+# Calculate the area of a circle
+import math
+
+def calculate_area_circle(radius):
+    area = math.pi * radius ** 2
+    return area
+
+# Test the function
+radius = input("Enter the radius of the circle: ")
+area = calculate_area_circle(radius)
+print("The area of the circle is:", area)
