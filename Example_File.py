1d3dd9a3a22a1f1b01771f1aede3e5d205740a46
Steven Ramsbottom <stevenrammy@hotmail.co.uk>
2024-03-16 01:00:06 +0000

    Changing to file with error

--- Example_File.py
+++ Example_File.py
@@ -10,8 +10,3 @@ def calculate_area_circle(radius):
 radius = input("Enter the radius of the circle: ")
 area = calculate_area_circle(radius)
 print("The area of the circle is:", area)
-=======
-print("Hello World!")
-print("This file goes in branch 1")
-print("I hope this works")
->>>>>>> 26f3cdedacb3715f94af515a82b692771863ddc6
