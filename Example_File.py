ac3c92c9d6649990f20eb3c99b95167058e50138
Steven Ramsbottom <stevenrammy@hotmail.co.uk>
2024-03-16 01:10:04 +0000

    Adding comments and making more readable

--- Example_File.py
+++ Example_File.py
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+""" This program calculates the area of a circle in cm squared"""
+
 # Calculate the area of a circle
 import math
 
@@ -7,6 +8,11 @@ def calculate_area_circle(radius):
     return area
 
 # Test the function
-radius = input("Enter the radius of the circle: ")
-area = calculate_area_circle(radius)
-print("The area of the circle is:", area)
+radius_str = input("Enter the radius of the circle (in cm): ") # Asked for unit (cm)
+try:
+    radius = float(radius_str)
+    area = calculate_area_circle(radius)
+    print("The area of the circle is: {:.2f} cm squared.".format(area)) # Rounded to 2 decimals and provided unit
+except ValueError:
+    print("Please enter a valid number for the radius.")
+    
